aad29ee900fefbc458e82d11eda849fcd45d7631
tshkuro <tshkuro@bostonsd.com>
2023-12-21 22:55:45 +0300

    Add Home page

--- BulkyWeb/BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers;
 
 [Area("Customer")]
 public class HomeController : Controller
 {
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IWebHostEnvironment _webHostEnv;
+
+    public HomeController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+        
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var productList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+        return View(productList);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var product = _unitOfWork.Product.GetFirstOfDefault(x => x.Id == id, includeProperties: "Category");
+        return View(product);
     }
 }
